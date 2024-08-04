544002b861923ea6867653a93cbe3ec755f2093e
Henri Kauhanen <nenahuak@gmail.com>
2024-08-04 12:08:14 +0200

    tiny bug in stats

--- R/stats.R
+++ R/stats.R
@@ -16,16 +16,16 @@ try(dir.create("../results/tables", recursive=TRUE))
 mod <- glm(Delta_pref~Typology, data10[data10$Dataset == "WALS", ], family=gaussian)
 emm <- emmeans(mod, "Typology")
 pem <- pairs(emm)
-pem %>% tidy %>% select(-term) %>% select(-null.value) %>% dust %>% sprinkle(cols=c("estimate", "std.error", "statistic", "adj.p.value"), round=5) %>% sprinkle(cols="contrast", replace=c("interacting vs. non-interacting", "interacting vs. unknown", "unknown vs. non-interacting"))
-write.csv(pem, file="../results/tables/pref_wals.csv", row.names=FALSE)
+out <- pem %>% tidy %>% select(-term) %>% select(-null.value) %>% dust %>% sprinkle(cols=c("estimate", "std.error", "statistic", "adj.p.value"), round=5) %>% sprinkle(cols="contrast", replace=c("interacting vs. non-interacting", "interacting vs. unknown", "unknown vs. non-interacting"))
+write.csv(out, file="../results/tables/pref_wals.csv", row.names=FALSE)
 
 
 # dispreferred types, WALS
 mod <- glm(Delta_dispref~Typology, data10[data10$Dataset == "WALS", ], family=gaussian)
 emm <- emmeans(mod, "Typology")
 pem <- pairs(emm)
-pem %>% tidy %>% select(-term) %>% select(-null.value) %>% dust %>% sprinkle(cols=c("estimate", "std.error", "statistic", "adj.p.value"), round=5) %>% sprinkle(cols="contrast", replace=c("interacting vs. non-interacting", "interacting vs. unknown", "unknown vs. non-interacting"))
-write.csv(pem, file="../results/tables/dispref_wals.csv", row.names=FALSE)
+out <- pem %>% tidy %>% select(-term) %>% select(-null.value) %>% dust %>% sprinkle(cols=c("estimate", "std.error", "statistic", "adj.p.value"), round=5) %>% sprinkle(cols="contrast", replace=c("interacting vs. non-interacting", "interacting vs. unknown", "unknown vs. non-interacting"))
+write.csv(out, file="../results/tables/dispref_wals.csv", row.names=FALSE)
 
 
 
