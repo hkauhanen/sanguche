5b3814eedd7cdb6eb188a98f14b66d8dabc694ee
Henri Kauhanen <nenahuak@gmail.com>
2024-08-04 12:14:23 +0200

    did grambank stats

--- R/stats.R
+++ R/stats.R
@@ -29,4 +29,21 @@ write.csv(out, file="../results/tables/dispref_wals.csv", row.names=FALSE)
 
 
 
+# preferred types, Grambank
+mod <- glm(Delta_pref~Typology, data10[data10$Dataset == "Grambank", ], family=gaussian)
+emm <- emmeans(mod, "Typology")
+pem <- pairs(emm)
+out <- pem %>% tidy %>% select(-term) %>% select(-null.value) %>% dust %>% sprinkle(cols=c("estimate", "std.error", "statistic", "adj.p.value"), round=5) %>% sprinkle(cols="contrast", replace=c("interacting vs. non-interacting", "interacting vs. unknown", "unknown vs. non-interacting"))
+write.csv(out, file="../results/tables/pref_grambank.csv", row.names=FALSE)
+
+
+# dispreferred types, Grambank
+mod <- glm(Delta_dispref~Typology, data10[data10$Dataset == "Grambank", ], family=gaussian)
+emm <- emmeans(mod, "Typology")
+pem <- pairs(emm)
+out <- pem %>% tidy %>% select(-term) %>% select(-null.value) %>% dust %>% sprinkle(cols=c("estimate", "std.error", "statistic", "adj.p.value"), round=5) %>% sprinkle(cols="contrast", replace=c("interacting vs. non-interacting", "interacting vs. unknown", "unknown vs. non-interacting"))
+write.csv(out, file="../results/tables/dispref_grambank.csv", row.names=FALSE)
+
+
+
 
