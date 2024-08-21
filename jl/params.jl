cc5eef1804d6db0c24db5a0f223f8d051ef6d5ae
Henri Kauhanen <nenahuak@gmail.com>
2024-08-21 21:43:26 +0200

    want all neighbourhood sizes between 1 and 200

--- jl/params.jl
+++ jl/params.jl
@@ -1,6 +1,7 @@
 # neighbourhood size (a quasilogarithmic sequence of integers from 1 to 200)
-degrees = Int.(unique(round.(exp.(range(log(1), stop=log(200), length=22)))))
+#degrees = Int.(unique(round.(exp.(range(log(1), stop=log(200), length=22)))))
 #degrees = [10]   ### uncomment for debugging purposes
+degrees = 1:200
 
 # WALS features we are interested in
 wo_features = ["87A", "86A", "85A", "88A", "89A", "83A", "90A", "82A"]
