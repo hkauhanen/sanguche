4f25e53cb0a602204f25695db243ae9738941abd
Henri Kauhanen <nenahuak@gmail.com>
2024-08-28 18:04:31 +0200

    should work now hopefully..

--- phylo/make_one.sh
+++ phylo/make_one.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
+make purge DATASET=$1
 make data DATASET=$1
-make revbayes DATASET=$1
 make mrbayes_small DATASET=$1
 make mrbayes_large DATASET=$1
+make revbayes DATASET=$1
 make posterior DATASET=$1
 make model DATASET=$1
 make correlations DATASET=$1
