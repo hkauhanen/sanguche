97d2f2dda92dbf23acba39dd9fd9c65e6e477c41
Henri Kauhanen <nenahuak@gmail.com>
2024-08-29 12:19:33 +0200

    no more separation into small and large families

--- phylo/make_one.sh
+++ phylo/make_one.sh
@@ -2,8 +2,7 @@
 
 make purge DATASET=$1
 make data DATASET=$1
-make mrbayes_small DATASET=$1
-make mrbayes_large DATASET=$1
+make mrbayes DATASET=$1
 make revbayes DATASET=$1
 make posterior DATASET=$1
 make model DATASET=$1
