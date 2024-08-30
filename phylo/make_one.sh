55546a8570492f44cd1d6da5623ae361d008d412
Henri Kauhanen <nenahuak@gmail.com>
2024-08-30 13:01:09 +0200

    execute mrbayes on 15 CPUs for WALS, 1 CPU + 1 GPU on Grambank, all simultaneously

--- phylo/make_one.sh
+++ phylo/make_one.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-make purge DATASET=$1
-make data DATASET=$1
-make mrbayes DATASET=$1
-make revbayes DATASET=$1
-make posterior DATASET=$1
-make model DATASET=$1
-make correlations DATASET=$1
+#make purge DATASET=$1           # runtime: negligible
+#make data DATASET=$1            # runtime: some minutes
+#make revbayes DATASET=$1        # runtime: some minutes, times 2 or 3
+make mrbayes_$1 DATASET=$1        # runtime: some days!!!
+#make posterior DATASET=$1      # runtime: some minutes
+#make model DATASET=$1          # runtime: ???
+#make correlations DATASET=$1   # runtime: some minutes
