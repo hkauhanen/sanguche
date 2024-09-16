50b8924a21e4dba90f22847a113e0cc14fec09c1
Henri Kauhanen <nenahuak@gmail.com>
2024-09-16 16:00:10 +0200

    update from office

--- phylo4/wals/code/model.sh
+++ phylo4/wals/code/model.sh
@@ -1,12 +1,12 @@
 cd modelFitting
 
-for i in {0..3}; do 
-  for j in {0..14}; do
+for i in {0..4}; do 
+  for j in {0..8}; do
     let "k = $i*10 + $j"
     #echo $k; sleep 2 &   ### debugging/testing
     julia +1.5.3 universal.jl $k &
   done
-  let "k = $i*10 + 15"
+  let "k = $i*10 + 9"
   #echo $k; sleep 2 ;     ### debugging/testing
   julia +1.5.3 universal.jl $k ;
 done
