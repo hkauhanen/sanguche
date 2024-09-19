73e8aa9d3d616029fb8e8858f5d792330b9460b5
Henri Kauhanen <nenahuak@gmail.com>
2024-09-19 18:07:12 +0200

    mrbayes_progress.sh now takes a cmd line argument as to the dataset

--- phylo/mrbayes_progress.sh
+++ phylo/mrbayes_progress.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/bash
 
-WALS_CONV=`ls wals/code/mrbayes/converged/*.txt -lh | wc -l`
-GRAM_CONV=`ls grambank/code/mrbayes/converged/*.txt -lh | wc -l`
+CONV=`ls $1/code/mrbayes/converged/*.txt -lh | wc -l`
 
-WALS_TOTL=`cat wals/data/glot3.txt | wc -l`
-GRAM_TOTL=`cat grambank/data/glot3.txt | wc -l`
+TOTL=`cat $1/data/glot3.txt | wc -l`
 
-echo "Families converged, WALS:     $WALS_CONV / $WALS_TOTL"
-echo "Families converged, Grambank: $GRAM_CONV / $GRAM_TOTL"
+echo "Families converged: $CONV / $TOTL"
 
 
