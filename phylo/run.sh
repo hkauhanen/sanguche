a6637362e6c90cb3f77206e17865a3e2b3f83d4a
Henri Kauhanen <nenahuak@gmail.com>
2024-10-01 12:37:48 +0200

    small tweak

--- phylo/run.sh
+++ phylo/run.sh
@@ -2,7 +2,8 @@
 
 #make data DATASET=$1
 make mrbayes_large DATASET=$1 NPROCS=3 & make mrbayes_problematic DATASET=$1 NPROCS=3 & make mrbayes_rest DATASET=$1 NPROCS=8
-#make revbayes DATASET=$1
+#make revbayes DATASET=$1 NPROCS=16
 #make posterior DATASET=$1
 #make model DATASET=$1
+#sleep 1h   # make sure model has finished for all feature pairs before proceeding to correlations
 #make correlations DATASET=$1
