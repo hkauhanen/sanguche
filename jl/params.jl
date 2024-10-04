2a2bd5772ebd78ea296e0cb36517cb43a6976dcc
Henri Kauhanen <nenahuak@gmail.com>
2024-10-04 14:28:10 +0200

    reverting back to old GB controls

--- jl/params.jl
+++ jl/params.jl
@@ -1,7 +1,7 @@
-# neighbourhood size (a quasilogarithmic sequence of integers from 1 to 200)
+# neighbourhood sizes
 #degrees = Int.(unique(round.(exp.(range(log(1), stop=log(200), length=22)))))
 #degrees = [10]   ### uncomment for debugging purposes
-degrees = 1:200
+degrees = 1:100
 
 # WALS features we are interested in
 wo_features = ["87A", "86A", "85A", "88A", "89A", "83A", "90A", "82A"]
@@ -11,7 +11,7 @@ features_wals = vcat(wo_features, control_features_wals)
 
 # Grambank features we are interested in
 wo_features = ["GB130", "GB065", "GB193", "GB025", "GB024", "VO", "NRc", "PN"]
-#control_features_grambank = ["GB030", "GB302"]
-control_features_grambank = ["GB059", "GB068"]
+control_features_grambank = ["GB030", "GB302"]
+#control_features_grambank = ["GB059", "GB068"]
 features_grambank = vcat(wo_features, control_features_grambank)
 
