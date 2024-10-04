135afee259ee34c30437175b59454ceeee8a4290
Henri Kauhanen <nenahuak@gmail.com>
2024-10-04 14:59:44 +0200

    wrapped logvisuals in a try-catch, so that we do not get an error when no family has yet converged

--- phylo/src/code/logvisuals.R
+++ phylo/src/code/logvisuals.R
@@ -3,40 +3,45 @@ require(reshape2)
 require(ggsci)
 
 
-files <- list.files(path="mrbayes/logs/", pattern="*.csv", full.names=TRUE)
+tryCatch(expr = {
+         files <- list.files(path="mrbayes/logs/", pattern="*.csv", full.names=TRUE)
 
-read_one <- function(X) {
-  df <- read.csv(X, header=FALSE)
-  names(df) <- c("family", "timestamp", "generations", "ASDSF", "maxPSRF")
-  df
-}
+         read_one <- function(X) {
+           df <- read.csv(X, header=FALSE)
+           names(df) <- c("family", "timestamp", "generations", "ASDSF", "maxPSRF")
+           df
+         }
 
-df <- do.call(rbind, lapply(X=files, FUN=read_one))
-df$dataset <- "WALS"
-df$converged <- FALSE
-df$smaxPSRF = df$maxPSRF/110
+         df <- do.call(rbind, lapply(X=files, FUN=read_one))
+         df$dataset <- "WALS"
+         df$converged <- FALSE
+         df$smaxPSRF = df$maxPSRF/110
 
-for (i in 1:nrow(df)) {
-  famhere = df[i, ]$family
-  if (file.exists(paste0("mrbayes/converged/", famhere, ".txt"))) {
-    df[i, ]$converged <- TRUE
-  }
-}
-
-
-df$lty <- ifelse(df$converged, "1", "2")
-df$alpha <- ifelse(df$converged, 0.5, 1.0)
+         for (i in 1:nrow(df)) {
+           famhere = df[i, ]$family
+           if (file.exists(paste0("mrbayes/converged/", famhere, ".txt"))) {
+             df[i, ]$converged <- TRUE
+           }
+         }
 
 
-df$converged <- ifelse(df$converged, "yes", "no")
-df$converged <- factor(df$converged, levels=c("no", "yes"))
+         df$lty <- ifelse(df$converged, "1", "2")
+         df$alpha <- ifelse(df$converged, 0.5, 1.0)
+
+
+         df$converged <- ifelse(df$converged, "yes", "no")
+         df$converged <- factor(df$converged, levels=c("no", "yes"))
 
 
 
-pdf("../../log.pdf", height=20, width=20)
+         pdf("../../log.pdf", height=20, width=20)
 
-g_w <- ggplot(melt(df, measure.vars=c("ASDSF", "smaxPSRF")), aes(lty=variable, x=generations, y=value, color=converged, group=interaction(variable, family))) + geom_path(lwd=1.0, show.legend=TRUE) + facet_wrap(.~family, scales="free", nrow=9, ncol=9) + geom_hline(yintercept=0.01, lty=1, alpha=0.5, lwd=1.0) + theme_bw() + scale_color_npg() + theme(legend.position="top") + scale_y_log10() + annotation_logticks(sides="l")
+         g_w <- ggplot(melt(df, measure.vars=c("ASDSF", "smaxPSRF")), aes(lty=variable, x=generations, y=value, color=converged, group=interaction(variable, family))) + geom_path(lwd=1.0, show.legend=TRUE) + facet_wrap(.~family, scales="free", nrow=9, ncol=9) + geom_hline(yintercept=0.01, lty=1, alpha=0.5, lwd=1.0) + theme_bw() + scale_color_npg() + theme(legend.position="top") + scale_y_log10() + annotation_logticks(sides="l")
 
-print(g_w)
+         print(g_w)
 
-dev.off()
+         dev.off()
+},
+error = function(e) {
+  print("nothing to do... no family has converged")
+})
