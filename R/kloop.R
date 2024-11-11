82be598a6785a5e081017300f325c1a4476e2c92
Henri Kauhanen <nenahuak@gmail.com>
2024-11-11 15:44:02 +0100

    global neighbourhood sizes based on sqrt(N)

--- R/kloop.R
+++ R/kloop.R
@@ -1,15 +1,13 @@
-kloop <- function(fulldata, data, dataset, variable = "Delta_under", indvariable = "status") {
-  fulldata$status <- relevel(fulldata$status, ref="non-interacting")
+kloop <- function(data, dataset, variable = "Delta_under", indvariable = "status") {
   data$status <- relevel(data$status, ref="non-interacting")
 
-  ks <- -10:10
+  ks <- 1:100
 
   df <- expand.grid(k=ks, estimate=NA, pval=NA, dataset=dataset)
 
   for (k in ks) {
-    datah <- fulldata[fulldata$dataset == dataset & fulldata$k == fulldata$inflpoint + k, ]
+    datah <- data[data$k == k & data$dataset == dataset, ]
     mod <- summary(lm(eval(parse(text=variable))~eval(parse(text=indvariable))+abs(phi), datah))
-
     df[df$k == k, ]$estimate <- mod$coefficients[2, "Estimate"]
     df[df$k == k, ]$pval <- mod$coefficients[2, "Pr(>|t|)"]
   }
