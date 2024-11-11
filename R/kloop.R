079c78478512a8c5befee5cf60f99213f4e014e0
Henri Kauhanen <nenahuak@gmail.com>
2024-11-11 16:04:13 +0100

    kloop.R now also does AIC difference in model 2

--- R/kloop.R
+++ R/kloop.R
@@ -3,13 +3,18 @@ kloop <- function(data, dataset, variable = "Delta_under", indvariable = "status
 
   ks <- 1:100
 
-  df <- expand.grid(k=ks, estimate=NA, pval=NA, dataset=dataset)
+  df <- expand.grid(k=ks, estimate=NA, pval=NA, DAIC=NA, dataset=dataset)
 
   for (k in ks) {
     datah <- data[data$k == k & data$dataset == dataset, ]
-    mod <- summary(lm(eval(parse(text=variable))~eval(parse(text=indvariable))+abs(phi), datah))
-    df[df$k == k, ]$estimate <- mod$coefficients[2, "Estimate"]
-    df[df$k == k, ]$pval <- mod$coefficients[2, "Pr(>|t|)"]
+
+    mod1 <- summary(lm(eval(parse(text=variable))~eval(parse(text=indvariable))+abs(phi), datah))
+    mod2a <- lm(eval(parse(text=variable))~phi, datah)
+    mod2b <- lm(eval(parse(text=variable))~corrected_phi, datah)
+    
+    df[df$k == k, ]$estimate <- mod1$coefficients[2, "Estimate"]
+    df[df$k == k, ]$pval <- mod1$coefficients[2, "Pr(>|t|)"]
+    df[df$k == k, ]$DAIC <- AIC(mod2a) - AIC(mod2b)
   }
 
   df
