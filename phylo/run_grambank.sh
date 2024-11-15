40cc4049c7f487f71cc4ec6b4ccbc9ff10f7abb7
Henri Kauhanen <nenahuak@gmail.com>
2024-11-15 17:40:07 +0100

    run script for grambank

---
+++ phylo/run_grambank.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+make data DATASET=grambank
+
+wait
+
+make revbayes DATASET=grambank NPROCS=8
+
+wait
+
+make mrbayes_rest DATASET=grambank NPROCS=9 \
+  & make mrbayes_large DATASET=grambank NPROCS=3
+
+wait
+
+make posterior DATASET=grambank
+
+make model DATASET=grambank
+
+wait
+
+make correlations DATASET=grambank
