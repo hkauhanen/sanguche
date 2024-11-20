08a43bf16864d4f91426901b0c89b175fefe5291
Henri Kauhanen <nenahuak@gmail.com>
2024-11-20 15:12:45 +0100

    minor bugfix with ggplot code

--- phylo/src/code/logvisuals.R
+++ phylo/src/code/logvisuals.R
@@ -37,7 +37,7 @@ tryCatch(expr = {
 
          pdf(paste0("../../log_", dataset, ".pdf"), height=20, width=20)
 
-         g_w <- ggplot(melt(df, measure.vars=c("ASDSF", "smaxPSRF")), aes(lty=variable, x=generations, y=value, color=converged, group=interaction(variable, family))) + geom_path(lwd=1.0, show.legend=TRUE) + facet_wrap(.~family, scales="free", nrow=9, ncol=9) + geom_hline(yintercept=0.01, lty=1, alpha=0.5, lwd=1.0) + theme_bw() + scale_color_npg() + theme(legend.position="top") + scale_y_log10() + annotation_logticks(sides="l")
+         g_w <- ggplot(melt(df, measure.vars=c("ASDSF", "smaxPSRF")), aes(lty=variable, x=generations, y=value, color=converged, group=interaction(variable, family))) + geom_path(lwd=1.0, show.legend=TRUE) + facet_wrap(.~family, scales="free", nrow=10, ncol=9) + geom_hline(yintercept=0.01, lty=1, alpha=0.5, lwd=1.0) + theme_bw() + scale_color_npg() + theme(legend.position="top") + scale_y_log10() + annotation_logticks(sides="l")
 
          print(g_w)
 
