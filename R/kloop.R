e86d56cc6fe04684df12aed35952313717594fa5
Henri Kauhanen <nenahuak@gmail.com>
2024-12-05 11:09:17 +0100

    updating after meeting

--- R/kloop.R
+++ R/kloop.R
@@ -1,16 +1,23 @@
 kloop <- function(data, dataset, variable = "Delta_under", indvariable = "status") {
-  data$status <- relevel(data$status, ref="non-interacting")
+  #data$status <- relevel(data$status, ref="non-interacting")
+  data <- data[data$status != "non-interacting", ]
 
-  ks <- 1:100
+  ks <- 1:500
 
   df <- expand.grid(k=ks, estimate=NA, pval=NA, DAIC=NA, dataset=dataset)
 
   for (k in ks) {
     datah <- data[data$k == k & data$dataset == dataset, ]
 
-    mod1 <- summary(lm(eval(parse(text=variable))~eval(parse(text=indvariable))+abs(phi), datah))
+    #mod1 <- summary(lm(eval(parse(text=variable))~eval(parse(text=indvariable))+abs(phi), datah))
+    #mod1 <- summary(lm(eval(parse(text=variable))~eval(parse(text=indvariable))+abs(phi), datah))
+    mod1 <- summary(lm(eval(parse(text=variable))~eval(parse(text=indvariable)), datah))
     mod2a <- lm(eval(parse(text=variable))~abs(phi), datah)
     mod2b <- lm(eval(parse(text=variable))~abs(corrected_phi), datah)
+
+    if (k == 1) {
+      print(mod1)
+    }
     
     df[df$k == k, ]$estimate <- mod1$coefficients[2, "Estimate"]
     df[df$k == k, ]$pval <- mod1$coefficients[2, "Pr(>|t|)"]
--- jl/params.jl
+++ jl/params.jl
@@ -1,7 +1,7 @@
 # neighbourhood sizes
 #degrees = Int.(unique(round.(exp.(range(log(1), stop=log(200), length=22)))))
 #degrees = [10]   ### uncomment for debugging purposes
-degrees = 1:100
+degrees = 1:500
 
 # WALS features we are interested in
 wo_features = ["87A", "86A", "85A", "88A", "89A", "83A", "90A", "82A"]
