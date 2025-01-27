280f4ee8fb9db19d4a584c927a8b872319bdd692
Henri Kauhanen <nenahuak@gmail.com>
2025-01-27 15:37:36 +0100

    ditto

--- src/code/savage_dickey.R
+++ src/code/savage_dickey.R
@@ -1,3 +1,6 @@
+args <- commandArgs(trailingOnly=TRUE)
+dataset <- args[1]
+
 if (!require(LRO.utilities)) {
   install.packages("devtools")
   devtools::install_github("LudvigOlsen/LRO.utilities")  
@@ -5,10 +8,10 @@ if (!require(LRO.utilities)) {
 
 library(LRO.utilities, quietly=TRUE)
 
-pr = read.csv("tmp/corr_prior.csv")
-po = read.csv("tmp/corr_posterior.csv")
+pr = read.csv(paste0("tmp/", dataset, "/corr_prior.csv"))
+po = read.csv(paste0("tmp/", dataset, "/corr_posterior.csv"))
 
 result = log(savage_dickey(post=po$posteriorCor, prior=pr$priorCor, Q=0, plot=FALSE)$BF01)
 
-write.csv(as.data.frame(result), "tmp/result.csv", row.names=FALSE)
+write.csv(as.data.frame(result), paste0("tmp/", dataset, "/result.csv"), row.names=FALSE)
 
