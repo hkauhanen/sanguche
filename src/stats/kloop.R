7c65af69a109da204fd69b4e3ed8e4be13cf7aba
Henri Kauhanen <nenahuak@gmail.com>
2025-08-24 21:25:17 +0200

    reran stats

--- src/stats/kloop.R
+++ src/stats/kloop.R
@@ -6,10 +6,7 @@ require(dplyr)
 kloop <- function(data, variable, ks = -30:50) {
 	var <- ifelse(variable == "underattested", "Delta_under", "Delta_over")
 
-	data$status <- factor(data$status)
-	data$status <- relevel(data$status, ref="non-interacting")
-
-	df <- expand.grid(k=ks, estimate=NA, effect.size=NA, pvalue=NA)
+	df <- expand.grid(k=ks, estimate=NA, effect.size=NA, effect.size.2=NA, effect.size.3=NA, pvalue=NA)
 
 	for (k in ks) {
 		datah <- data %>% group_by(pair) %>% filter(degree == round(sqrt(N)) + k)
@@ -22,9 +19,11 @@ kloop <- function(data, variable, ks = -30:50) {
 		con <- as.data.frame(emm$contrasts)
 		eff <- as.data.frame(eff_size(emm, sigma=sigma(mod), edf=mod$df, method="identity"))
 
-		df[df$k == k, ]$estimate = -con[con$contrast == paste0("(", var, " non-interacting) - ", var, " interacting"), ]$estimate
-		df[df$k == k, ]$effect.size = -eff[eff$contrast == paste0("((", var, " non-interacting) - ", var, " interacting)"), ]$effect.size
-		df[df$k == k, ]$pvalue = con[con$contrast == paste0("(", var, " non-interacting) - ", var, " interacting"), ]$p.value
+		df[df$k == k, ]$estimate = con[con$contrast == paste0("", var, " control - ", var, " interacting"), ]$estimate
+		df[df$k == k, ]$effect.size = eff[eff$contrast == paste0("(", var, " control - ", var, " interacting)"), ]$effect.size
+		df[df$k == k, ]$effect.size.2 = eff[eff$contrast == paste0("(", var, " control - ", var, " unknown)"), ]$effect.size
+		df[df$k == k, ]$effect.size.3 = eff[eff$contrast == paste0("(", var, " interacting - ", var, " unknown)"), ]$effect.size
+		df[df$k == k, ]$pvalue = con[con$contrast == paste0("", var, " control - ", var, " interacting"), ]$p.value
 		#df[df$k == k, ]$estimate = con[con$contrast == paste0(var, " interacting - (", var, " non-interacting)"), ]$estimate
 		#df[df$k == k, ]$pvalue = con[con$contrast == paste0(var, " interacting - (", var, " non-interacting)"), ]$p.value
 	}
